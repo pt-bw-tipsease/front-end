89a6577fda0adab87473fe15e39d9868a6514f08
Th5tch3r <amber.phan27@gmail.com>
2019-08-29 22:55:36 -0400

    sign up page funtioning

--- tipsease-front-end/src/components/SignUp.js
+++ tipsease-front-end/src/components/SignUp.js
@@ -1,52 +1,124 @@
 import React from 'react';
-import { Form, Field, withFormik } from 'formik';
-import * as Yup from "yup";
+import {NavLink} from 'react-router-dom';
 import axios from "axios";
 
+export default class SignUpForm extends React.Component{
+    state= {
+        fullName: "",
+        photoUrl: "",
+        serviceType: "",
+        workplace: "",
+        timeAtJob: "",
+        tagline: "",
+        bio: "",
+        username: "",
+        password: "",
+    }
 
+    changeHandler = event => {
+        this.setState({ [event.target.name]: event.target.value})
+    }
 
-// export default function SignUp(){
-//     return(
-//             <form>
-//                     <input
-//                         fluid label="First Name"
-//                         placeholder="First Name"
-//                     />
-//                     <input
-//                         fluid label="Last Name"
-//                         placeholder="Last Name"
-//                     />
+    handleSubmit = event => {
+        event.preventDefault();
+        const newUser ={
+            username: this.state.username,
+            password: this.state.password,
+            fullName: this.state.fullName,
+            serviceType: this.state.serviceType,
+            workplace: this.state.workplace,
+            timeAtJob: this.state.timeAtJob,
+            tagline: this.state.tagline,
+            bio: this.state.bio,
 
-//                 <button type="submit">Sign Up</button>
-//             </form>
+        };
 
-//     )
-// }
+        console.log(newUser);
+        axios.post(`https://vr-overlord-server.herokuapp.com/serviceWorkers`, newUser)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                console.log("account created!")
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
-const SignUpForm = () => {
+    render(){
+
     return(
         <div>
-            <Form>
-                <Field
-                component="input"
+            <form onSubmit={this.handleSubmit}>
+
+            <label htmlFor="fullName">Full Name</label>
+            <input
                 type="text"
-                name="fullname"
-                placeholder="Fullname"
-                />
-            </Form>
+                name="fullName"
+                placeholder="Full Name"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="serviceType">Service Type</label>
+            <input
+                type="text"
+                name="serviceType"
+                placeholder="Service Type"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="workplace">Workplace</label>
+            <input
+                type="text"
+                name="workplace"
+                placeholder="Company Name"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="timeAtJob">Time At Job</label>
+            <input
+                type="number"
+                name="timeAtJob"
+                placeholder="Length of Service(in months)"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="tagline">$Cashtag</label>
+            <input
+                type="text"
+                name="tagline"
+                placeholder="Tagline"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="bio">Bio</label>
+            <textarea
+                name="bio"
+                placeholder="Tell something about yourself"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="username">Username</label>
+            <input
+                type="text"
+                name="username"
+                placeholder="username"
+                onChange={this.changeHandler}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.changeHandler}
+            />
+
+
+            <button type="submit">Submit!</button>
+            </form>
+            <h3>Already have account?</h3>
+            <NavLink to="/login"><button>Login</button></NavLink>
         </div>
     )
-
-}
-
-const formikHOC = withFormik({
-    mapPropsToValues({ fullname }) {
-        return {
-            fullname: fullname || "",
-        }
-    }
-})
-
-const SignUpFormWithFormik = formikHOC(SignUpForm);
-
-export default SignUpFormWithFormik;
+}}
